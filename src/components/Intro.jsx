5c2ce54c55ef29204edfd82fd31c2e8f0d580d67
bastien-app <bastien.goumy@outlook.fr>
2025-10-31 00:53:53 +0100

    ajout premier composant

---
+++ src/components/Intro.jsx
@@ -0,0 +1,13 @@
+function Intro ()
+{
+    return (
+        <div>
+            <h1 className="nom-intro">Bastien Goumy</h1>
+            <h2>Concepteur développeur d'applications</h2>
+            <button>Démarrer</button>
+        </div>
+    )
+}
+
+
+export default Intro
