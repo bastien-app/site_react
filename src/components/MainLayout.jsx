92c0a4a034c3d4bc837ed7f087378978b5df9258
bastien-app <bastien.goumy@outlook.fr>
2025-10-31 19:35:19 +0100

    Continuation menu

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -3,12 +3,18 @@ import Menu from "./Menu";
 
 
 function Main(){
-    const [activeSection, setActiveSection] = useState("")
+    const [activeSection, setActiveSection] = useState("about")
+
+    function onSelect(sectionId){
+        setActiveSection(sectionId)
+    }
 
     return(
         <div>
-            <p>Test</p>
+            <Menu active={activeSection} onSelect={onSelect}/>
         </div>
+            
+        
     )
 }
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,13 +1,59 @@
-function Menu()
+function Menu({active, onSelect})
 {
     return(
         <nav aria-label="Menu principal" className="menu">
             <ul>
-                <li><a>À propos</a></li>
-                <li><a>L'école</a></li>
-                <li><a>Compétences</a></li>
-                <li><a>Projets et réalisations</a></li>
-                <li><a>Contact</a></li>
+                <li>
+                    <a
+                        href="#about"
+                        className={active === "about" ? "active" : ""}
+                        onClick={() => onSelect("about")}
+                    >
+                        À propos
+                    </a>
+                    </li>
+
+                <li>
+                    <a
+                        href="#school"
+                        className={active === "school" ? "active" : ""}
+                        onClick={() => onSelect("school")}
+                    >
+                        L'école
+                    </a>
+                    </li>
+
+                <li>
+                    <a
+                        href="#skills"
+                        className={active === "skills" ? "active" : ""}
+                        onClick={() => onSelect("skills")}
+                    
+                    >
+                        Compétences
+                    </a>
+                    </li>
+
+                <li>
+                    <a
+                        href="#projects"
+                        className={active === "projects" ? "active" : ""}
+                        onClick={() => onSelect("projects")}
+                    
+                    >
+                        Projets et réalisations
+                    </a>
+                    </li>
+                    
+                <li>
+                    <a
+                        href="#contact"
+                        className={active === "contact" ? "active" : ""}
+                        onClick={() => onSelect("contact")}
+                    >
+                        Contact
+                    </a>
+                    </li>
             </ul>
         </nav>
     )
